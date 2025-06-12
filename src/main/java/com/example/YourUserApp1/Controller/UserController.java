333cfc2473b0fb52c617bd9b25511b54c4337e9e
NodStan <stanleyobitunwase@gmail.com>
2025-06-12 23:31:51 +0100

    4 commit

--- src/main/java/com/example/YourUserApp1/Controller/UserController.java
+++ src/main/java/com/example/YourUserApp1/Controller/UserController.java
@@ -1,97 +1,3 @@
-//package com.example.YourUserApp1.Controller;
-//
-//import com.example.YourUserApp1.Model.User;
-//import com.example.YourUserApp1.Repository.UserRepository;
-//import com.example.YourUserApp1.UserService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.web.bind.annotation.*;
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/home")
-//public class UserController {
-//    @Autowired
-//    private UserRepository userRepository;
-//
-//    @Autowired
-//    public UserController(UserService userService) {
-//        this.userService = userService;
-//    }
-//
-//    @Autowired
-//    private BCryptPasswordEncoder passwordEncoder;
-//
-//    @GetMapping("/users")
-//    public List<User> getAllUsers() {
-//        return userRepository.findAll();
-//    }
-//
-//    @PostMapping("/createuser")
-//    public User createUser(@RequestBody User user) {
-//        user.setPassword(passwordEncoder.encode(user.getPassword())); // Encrypt password before saving
-//        return userRepository.save(user);
-//    }
-//
-//    private UserService userService;
-//
-//    @DeleteMapping("/deleteall")
-//    public String deleteAllUsers() {
-//        userService.deleteAllUsers();
-//        return "All users deleted successfully!";
-//    }
-//}
-
-
-//
-//
-//package com.example.YourUserApp1.Controller;
-//
-//import com.example.YourUserApp1.Model.User;
-//import com.example.YourUserApp1.UserService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/home")
-//public class UserController {
-//    private final UserService userService;
-//    private final BCryptPasswordEncoder passwordEncoder;
-//
-//    @Autowired
-//    public UserController(UserService userService, BCryptPasswordEncoder passwordEncoder) {
-//        this.userService = userService;
-//        this.passwordEncoder = passwordEncoder;
-//    }
-//
-//    @GetMapping("/users")
-//    public List<User> getAllUsers() {
-//        return userService.getAllUsers();
-//    }
-//
-//    @PostMapping("/createuser")
-//    public User createUser(@RequestBody User user) {
-//        user.setPassword(passwordEncoder.encode(user.getPassword())); // Encrypt password before saving
-//        return userService.createUser(user);
-//    }
-//
-//    @DeleteMapping("/deleteall")
-//    public String deleteAllUsers() {
-//        userService.deleteAllUsers();
-//        return "All users deleted successfully!";
-//    }
-//
-//    @DeleteMapping("/delete/{id}")
-//    public String deleteUserById(@PathVariable String id) {
-//        userService.deleteUserById(id);
-//        return "User with ID " + id + " deleted successfully!";
-//    }
-//}
-
-
 package com.example.YourUserApp1.Controller;
 
 import com.example.YourUserApp1.Model.User;
@@ -108,11 +14,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+@CrossOrigin(
+        origins = {
+                "https://healthcaresyst.netlify.app",
+                "http://localhost:5175"
+        },
+        allowCredentials = "true"
+)
 @RestController
 @RequestMapping("/home")
 public class UserController {
 
     record Task(int id, String task, String status){}
+
     private final UserService userService;
     private final BCryptPasswordEncoder passwordEncoder;
 
@@ -166,7 +80,6 @@ public class UserController {
         return ResponseEntity.ok(savedUser);
     }
 
-
     @PostMapping("/login")
     public ResponseEntity<?> loginUser(@RequestBody Map<String, String> loginData) {
         String email = loginData.get("email");
@@ -216,11 +129,6 @@ public class UserController {
         return "User with email " + email + " deleted successfully!";
     }
 
-//    @PutMapping("/update/{id}")
-//    public User updateUser(@PathVariable String id, @RequestBody User user) {
-//        return userService.updateUser(id, user);
-//    }
-
     @PutMapping("/update/{username}")
     public ResponseEntity<?> updateUser(@PathVariable String username, @RequestBody User updatedUser) {
         try {
@@ -234,5 +142,4 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error updating user");
         }
     }
-
 }
